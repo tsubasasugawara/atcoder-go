33b8015b1c8b65fc7f60a5262660882d8f49d1e8
tsubasasugawara <sugawara1740@gmail.com>
2023-02-16 23:30:26 +0900

    Added abc277

--- src/abc278/b/main.go
+++ src/abc278/b/main.go
@@ -6,6 +6,18 @@ import (
 	"bufio"
 )
 
+func isMissJudge(h int, m int) bool {
+	return (h % 10) >= 6 || ((m / 10) + (h / 10) * 10) >= 24
+}
+
+func next(h int, m int) (int, int) {
+	if m == 59 {
+		return (h + 1) % 24, 0
+	} else {
+		return h, m + 1 % 24
+	}
+}
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -13,4 +25,10 @@ func main() {
 
 	var h, m int
 	fmt.Fscan(in, &h, &m)
+
+	for isMissJudge(h, m) {
+		h, m = next(h, m)
+	}
+
+	fmt.Fprintf(out, "%d %d\n", h, m)
 }
--- src/abc278/c/main.go
+++ src/abc278/c/main.go
@@ -10,4 +10,30 @@ func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-}
+
+	var n, q int
+	fmt.Fscan(in, &n, &q)
+
+	following := make(map[string]bool)
+
+	for i := 0; i < q; i++ {
+		var t int
+		var a, b string
+		fmt.Fscan(in, &t, &a, &b)
+
+		switch t {
+		case 1:
+			following[a + " " + b] = true
+			break
+		case 2:
+			following[a+" "+b] = false
+			break
+		case 3:
+			if following[a+" "+b] && following[b+" "+a] {
+				fmt.Fprintln(out, "Yes")
+			} else {
+				fmt.Fprintln(out, "No")
+			}
+		}
+	}
+ }
